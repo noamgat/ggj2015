e40ea83263f9b16d1498127a269f4b1aa9299b75
Noam Gat <noamgat@gmail.com>
2015-01-23 14:11:38 +0200

    Added first version of minigame interface

---
+++ UnityProject/Assets/Scripts/MiniGame.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class MiniGame : MonoBehaviour {
+    public Camera camera;
+
+    [System.Serializable]
+    public class MiniGameEvent : UnityEvent<MiniGame> { }
+    public MiniGameEvent onLostLife;
+}
