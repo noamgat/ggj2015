f307163c64380d6dcdd323b40eed1a9078dca807
Noam Gat <noamgat@gmail.com>
2015-01-23 22:23:52 +0200

    Multi game now progresses and adds games as time passes

--- UnityProject/Assets/Scripts/MultiGame.cs
+++ UnityProject/Assets/Scripts/MultiGame.cs
@@ -1,26 +1,88 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MultiGame : MonoBehaviour {
 
     public MiniGame[] miniGamePrefabs;
     private MiniGame[] miniGames;
     public int numLives = 5;
-    public Rect[] minigameViewports;
+    public float transitionDuration = 2f;
+
+    [System.Serializable]
+    public class StageConfiguration
+    {
+        public Rect[] minigameViewports;
+        public float timeInStage;
+    }
+
+    public StageConfiguration[] stageConfigurations;
+    private float startTime;
+    private float currentStage;
 
 	// Use this for initialization
 	void Start () {
-        if (miniGamePrefabs.Length != minigameViewports.Length) {
-            Debug.LogError("Minigame <-> viewport config mismatch");
-        }
+        startTime = Time.realtimeSinceStartup;
         miniGames = new MiniGame[miniGamePrefabs.Length];
-        for (int i = 0; i < miniGames.Length; i++) {
-            miniGames[i] = GameObject.Instantiate(miniGamePrefabs[i]) as MiniGame;
-            miniGames[i].mainCamera.rect = minigameViewports[i];
-            miniGames[i].onLostLife.AddListener(this.OnMiniGameLostLife);
-            miniGames[i].NotifyLifeTotalChanged(numLives);
-        }
+        currentStage = GetCurrentStage();
+        UpdateStagesFromConfiguration();
 	}
+
+    private float GetCurrentStage() {
+        float timeLeftToCount = Time.realtimeSinceStartup - startTime;
+        int baseStage = 0;
+        float currentStage = 0;
+        while (baseStage < stageConfigurations.Length - 1) {
+            if (timeLeftToCount < stageConfigurations[baseStage].timeInStage) {
+                break;
+            }
+            timeLeftToCount -= stageConfigurations[baseStage].timeInStage;
+            if (timeLeftToCount > transitionDuration) {
+                currentStage += 1f;
+                baseStage++;
+                timeLeftToCount -= transitionDuration;
+            } else {
+                currentStage += (timeLeftToCount / transitionDuration);
+                break;
+            }
+        }
+        return currentStage;
+    }
+    void Update() {
+        float newCurrentStage = GetCurrentStage();
+        if (!Mathf.Approximately(currentStage, newCurrentStage)) {
+            currentStage = newCurrentStage;
+            UpdateStagesFromConfiguration();
+        }
+    }
+
+    private void UpdateStagesFromConfiguration() {
+        int baseStage = Mathf.FloorToInt(currentStage);
+        bool inTransition = !Mathf.Approximately(baseStage, currentStage);
+        StageConfiguration config = stageConfigurations[baseStage];
+        for (int i = 0; i < config.minigameViewports.Length; i++) {
+            if (miniGames[i] == null) {
+                miniGames[i] = GameObject.Instantiate(miniGamePrefabs[i]) as MiniGame;
+                miniGames[i].mainCamera.rect = config.minigameViewports[i];
+                miniGames[i].onLostLife.AddListener(this.OnMiniGameLostLife);
+                miniGames[i].NotifyLifeTotalChanged(numLives);
+            } else {
+                Rect rect;
+                if (inTransition) {
+                    float relativeProgress = Mathf.Repeat(currentStage, 1f);
+                    Rect srcRect = config.minigameViewports[i];
+                    Rect dstRect = stageConfigurations[baseStage + 1].minigameViewports[i];
+                    rect = new Rect(Mathf.Lerp(srcRect.xMin, dstRect.xMin, relativeProgress),
+                            Mathf.Lerp(srcRect.yMin, dstRect.yMin, relativeProgress),
+                            Mathf.Lerp(srcRect.width, dstRect.width, relativeProgress),
+                            Mathf.Lerp(srcRect.height, dstRect.height, relativeProgress));
+                } else {
+                    rect = config.minigameViewports[i];
+                }
+                miniGames[i].mainCamera.rect = rect;
+            }
+        }
+    }
 
     private void OnMiniGameLostLife(MiniGame miniGame) {
         numLives--;
