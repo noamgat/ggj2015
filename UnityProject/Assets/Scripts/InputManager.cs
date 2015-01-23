83ba9257c4359f747dc11f9825faf6b8c4f575aa
Amit Arnon <amit@7egames.com>
2015-01-23 15:20:22 +0200

    Added Input Manager Added MiniGameRacer first version

---
+++ UnityProject/Assets/Scripts/InputManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+    public static bool GetButtonDown { get; private set; }
+    public static bool GetButtonUp { get; private set; }
+    public static bool GetButtonHeld { get; private set; }
+
+	// Update is called once per frame
+	void Update () {
+        
+        GetButtonDown = false;
+        GetButtonUp = false;
+        GetButtonHeld = false;
+
+
+        if (Input.GetButtonDown("Fire1") || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            GetButtonDown = true;
+        }
+
+        if (Input.GetButtonUp("Fire1") || (Input.touchCount > 0 && ((Input.GetTouch(0).phase == TouchPhase.Ended) || (Input.GetTouch(0).phase == TouchPhase.Canceled))))
+        {
+            GetButtonUp = true;
+        }
+
+        if (Input.GetButton("Fire1") || (Input.touchCount > 0 && ((Input.GetTouch(0).phase == TouchPhase.Stationary) || (Input.GetTouch(0).phase == TouchPhase.Moved))))
+        {
+            GetButtonHeld = true;
+        }
+	}
+}
---
+++ UnityProject/Assets/Scripts/Racer/RacerManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RacerManager : MonoBehaviour {
+
+    public enum Position { Right, Left }
+
+    Position carPosition = Position.Right;
+    Position lastCarPosition = Position.Right;
+
+    public GameObject car;
+
+    public Transform leftPosition;
+    public Transform rightPosition;
+
+    void Start()
+    {
+        car.transform.position = rightPosition.position;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (InputManager.GetButtonDown)
+        {
+            if (carPosition == Position.Left) carPosition = Position.Right; else carPosition = Position.Left;
+        }
+
+        if (carPosition != lastCarPosition)
+        {
+            if (carPosition == Position.Left)
+                car.transform.position = leftPosition.position;
+            else
+                car.transform.position = rightPosition.position;
+            lastCarPosition = carPosition;
+        }
+	}
+}
