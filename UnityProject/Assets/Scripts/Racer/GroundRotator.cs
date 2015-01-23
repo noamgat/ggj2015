f1640a6433b5d57fcb6e9581604ff8e3c932b3d9
Amit Arnon <amit@7egames.com>
2015-01-23 18:37:28 +0200

    Improvements in Racer Added skyboxes

---
+++ UnityProject/Assets/Scripts/Racer/GroundRotator.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GroundRotator : MonoBehaviour {
+
+    public Transform rotatePoint;
+    public float speed;
+
+    public Transform rotateToPoint;
+
+    List<Transform> groundObjects = new List<Transform>();
+    Vector3 startPos;
+    Vector3 endPos;
+
+    void Awake()
+    {
+        startPos = rotateToPoint.position;
+        endPos = rotatePoint.position;
+        foreach (Transform child in transform)
+        {
+            groundObjects.Add(child);
+        }
+    }
+
+    void Update()
+    {
+        foreach (Transform obj in groundObjects)
+        {
+            obj.Translate(0, 0, -1 * speed * Time.deltaTime);
+            if (obj.position.z <= endPos.z)
+            {
+                obj.position = startPos - new Vector3(-obj.position.x,-obj.position.y,endPos.z - obj.position.z);
+            }
+        }
+    }
+}
