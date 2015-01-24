4c408231ead907ac9f8b069eca040d4e398bd6b8
Amit Arnon <amit@7egames.com>
2015-01-24 11:19:24 +0200

    Added preparation for procgen New models

--- UnityProject/Assets/Scripts/Racer/RacerManager.cs
+++ UnityProject/Assets/Scripts/Racer/RacerManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class RacerManager : MonoBehaviour {
 
@@ -18,14 +19,22 @@ public class RacerManager : MonoBehaviour {
 
     public MiniGame miniGame;
 
+    public List<GameObject> obstaclePrefabs;
+    public float minTimeToSpawn;
+    public float maxTimeToSpawn;
+    float lastTimeOfSpawn;
+    float nextTimeOfSpawnInterval;
+
     void Start()
     {
         car.transform.position = rightPosition.position;
         targetPosition = car.transform.position;
+        lastTimeOfSpawn = Time.time;
+        nextTimeOfSpawnInterval = GetNextIntervalTime();
     }
 
-	// Update is called once per frame
 	void Update () {
+        // Set new position on click
         if (InputManager.GetButtonDown)
         {
             if (carPosition == Position.Left) carPosition = Position.Right; else carPosition = Position.Left;
@@ -40,6 +49,15 @@ public class RacerManager : MonoBehaviour {
             lastCarPosition = carPosition;
         }
 
+        // Move towards new position
         car.transform.position = new Vector3(Mathf.Lerp(car.transform.position.x, targetPosition.x, Time.deltaTime * switchSpeed), car.transform.position.y, car.transform.position.z);
+
+        // Generate new obstacles
+        
 	}
+
+    float GetNextIntervalTime()
+    {
+        return Random.Range(minTimeToSpawn, maxTimeToSpawn);
+    }
 }
