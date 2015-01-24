87b96e9591446ae2c01c80368ccde721a69922d7
Noam Gat <noamgat@gmail.com>
2015-01-24 12:45:42 +0200

    Adding music!

---
+++ UnityProject/Assets/Scripts/SoundPlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundPlayer : MonoBehaviour {
+
+    public AudioSource introSource;
+    public AudioSource loopSource;
+
+	// Update is called once per frame
+	void Start () {
+        introSource.Play();
+        this.ExecuteWithDelay(introSource.clip.length, delegate() { loopSource.Play(); });
+	}
+}
