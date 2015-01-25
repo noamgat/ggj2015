27fb6b54fbae52089358a28e66376f6f7d5bf538
Noam Gat <noamgat@gmail.com>
2015-01-25 09:26:15 +0200

    Fixed sound transition between intro and loop

--- UnityProject/Assets/Scripts/SoundPlayer.cs
+++ UnityProject/Assets/Scripts/SoundPlayer.cs
@@ -5,10 +5,10 @@ public class SoundPlayer : MonoBehaviour {
 
     public AudioSource introSource;
     public AudioSource loopSource;
-
+    public float sharedBufferTime = 0.1f;
 	// Update is called once per frame
 	void Start () {
         introSource.Play();
-        this.ExecuteWithDelay(introSource.clip.length, delegate() { loopSource.Play(); });
+        this.ExecuteWithDelay(introSource.clip.length - sharedBufferTime, delegate() { loopSource.Play(); });
 	}
 }
